d831b3dce0808078ba152f735914ac08d58ead12
Fluffy-Lion <leoncooper31@gmail.com>
2021-09-21 21:23:54 +0100

    added chinese

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("Matrix");
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const katakana =
   "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const chinese = "書书長长當当韋韦樂乐車车興兴發发東东專专過过報报爾尔盡尽學学";
 const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const nums = "0123456789";
 
-const alphabet = katakana + latin + nums;
+const alphabet = katakana + latin + nums + chinese;
 
 const fontSize = 16;
 const columns = canvas.width / fontSize;
@@ -31,10 +32,10 @@ const draw = () => {
     const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
     context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
     if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-        rainDrops[i] = 0;
+      rainDrops[i] = 0;
     }
     rainDrops[i]++;
-}
+  }
 };
 
 setInterval(draw, 30);
